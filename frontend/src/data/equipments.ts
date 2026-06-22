1b5d78a05075c5a61a4ad96f973f782ef7894c99
David Silva <david11augusto11@gmail.com>
2026-06-22 16:35:36 -0400

    feat: estrutura de pastas

--- frontend/src/data/equipments.ts
+++
@@ -1,7 +0,0 @@
-import type { Equipments } from "../types/equipments.types"
-
-export const equipments: Equipments[] = [
-        {id: 1, name: "Projector", status: true},
-        {id: 2, name: "Laptop", status: false},
-        {id: 3, name: "Printer", status: true}
-    ]
--- frontend/src/types/equipments.types.ts
+++
@@ -1,5 +0,0 @@
-export interface Equipments{
-    id: number,
-    name: string,
-    status: boolean
-}
