9ee19726539d0c6b8ec1e85be8065d036ad11c67
David Silva <david11augusto11@gmail.com>
2026-07-01 21:30:40 -0400

    feat: add EquipmentDetailsPage navigation

--- frontend/src/features/equipment/pages/EquipmentPage/index.tsx
+++ frontend/src/features/equipment/pages/EquipmentPage/index.tsx
@@ -17,6 +17,7 @@ import {
 } from '../../mocks/equipment.mock'
 import type { Equipment, EquipmentStatus, EquipmentType } from '../../types/equipment'
 import { Container } from './styles'
+import { useNavigate } from 'react-router-dom'
 
 export function EquipmentPage() {
   const [messageApi, contextHolder] = message.useMessage()
@@ -30,6 +31,8 @@ export function EquipmentPage() {
   const [equipmentInForm, setEquipmentInForm] = useState<Equipment>()
   const [equipmentInStatus, setEquipmentInStatus] = useState<Equipment>()
   const [equipmentToRemove, setEquipmentToRemove] = useState<Equipment>()
+  const navigate = useNavigate()
+
 
   function handleCreateEquipment() {
     setFormMode('create')
@@ -75,6 +78,10 @@ export function EquipmentPage() {
     setSelectedType(undefined)
   }
 
+  function handleViewEquipment(equipment: Equipment){
+    navigate(`/equipment/${equipment.id}`)
+  }
+
   // AULA 05 - parte prática:
   // Primeiro deixamos a lista sem filtro para a tela aparecer.
   const visibleEquipment = equipmentMock
@@ -109,6 +116,7 @@ export function EquipmentPage() {
           onChangeStatusEquipment={setEquipmentInStatus}
           onEditEquipment={handleEditEquipment}
           onRemoveEquipment={setEquipmentToRemove}
+          onViewEquipment={handleViewEquipment}
         />
 
         <EquipmentFormModal
